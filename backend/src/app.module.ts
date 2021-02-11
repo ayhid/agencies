4ee90b7e208196f0925522a9000afc1c4f73c9f1
Ayoub Hidri <ayoub.hidri@isobar.com>
2021-02-11 11:21:37 +0100

    remove unused controller from scaffolder

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,12 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AgenciesController } from './agencies/agencies.controller';
 import { AgenciesService } from './agencies/agencies.service';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
 @Module({
   imports: [],
-  controllers: [AppController, AgenciesController],
-  providers: [AppService, AgenciesService],
+  controllers: [AgenciesController],
+  providers: [AgenciesService],
 })
 export class AppModule {}
